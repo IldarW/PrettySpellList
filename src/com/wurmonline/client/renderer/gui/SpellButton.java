9ef4dd0d7160377772e904e001e5f13d2d258105
IldarW <ildarwurm@gmail.com>
2019-01-15 20:00:14 +0300

    remove label rendering on spell button

--- src/com/wurmonline/client/renderer/gui/SpellButton.java
+++ src/com/wurmonline/client/renderer/gui/SpellButton.java
@@ -23,27 +23,18 @@ public class SpellButton extends WButton {
     @Override
     protected void renderComponent(Queue queue, final float alpha) {
         if (!this.hoverMode || this.hovered) {
-
             float r2 = this.r;
             float g2 = this.g;
             float b2 = this.b;
-
             if ((this.isCloseHovered || this.isDown) && this.isEnabled()) {
-
                 r2 /= 2.0f;
                 g2 /= 2.0f;
                 b2 /= 2.0f;
             }
-
             if (this.texture != null) {
                 this.drawTexture(queue, this.texture, r2, g2, b2, 1.0f, this.x, this.y, this.width, this.height, 0, 0, 256, 256);
             }
         }
-
-        final int yo2 = ((this.isCloseHovered || this.isDown) && this.isEnabled()) ? 1 : 0;
-        final float c = ((this.isCloseHovered || this.isDown) && this.isEnabled()) ? 0.8f : 1.0f;
-        this.text.moveTo(this.x + 4 + 0, this.y + this.text.getHeight() + yo2 + 0);
-        this.text.paint(queue, this.label, this.rText * c, this.gText *c, this.bText * c, 1.0f);
     }
 
 }
